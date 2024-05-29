b15b86816f881ccd528339d789c2be7b065f481e
Randy Binondo <binondord@gmail.com>
2024-05-30 04:15:42 +0800

    update score and level

--- Game/PacMan/src/main/java/com/example/pacman/LeaderBoard/Update.java
+++ Game/PacMan/src/main/java/com/example/pacman/LeaderBoard/Update.java
@@ -5,13 +5,14 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Update {
-    private static final String SQL_UPDATE = "UPDATE leaderboard SET playerName = ?, score = ? WHERE id = ?";
+    private static final String SQL_UPDATE = "UPDATE leaderboard SET playerName = ?, score = ?, level = ? WHERE id = ?";
 
     public void updateLeaderboard(Connection connection, int id, String name, int score) throws SQLException {
         try (PreparedStatement statement = connection.prepareStatement(SQL_UPDATE)) {
             statement.setString(1, name);
             statement.setInt(2, score);
-            statement.setInt(3, id);
+            statement.setInt(3, score);
+            statement.setInt(4, id);
             statement.executeUpdate();
         }
     }
